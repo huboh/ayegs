6d8285df20a4fd3acb3d474a96b14b5fc711fc35
Huboh <72440222+huboh@users.noreply.github.com>
2021-12-19 20:44:19 +0100

    😥 fix : use react-query callbacks rather than using my own with useEffect

--- src/hooks/useCartItems/cartItemUtils.ts
+++ src/hooks/useCartItems/cartItemUtils.ts
@@ -1,25 +1,17 @@
-import { useEffect } from "react";
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions } from "react-query";
 import { CartItem, CartItems } from "../../types";
 
 interface GetCartItemsProps {
-  onError(error: unknown): void;
-  onSuccess(cartItems: CartItems): void;
+  onError: UseQueryOptions<unknown>['onError'];
+  onSuccess: UseQueryOptions<CartItems>['onSuccess'];
 }
 
 const fetchCartItems = async (): Promise<CartItems> => [{ cart: '😋😋' }] as any;
 
 export const useGetCartItems = ({ onSuccess, onError }: GetCartItemsProps) => {
-  const queryResults = useQuery('cart-items', fetchCartItems);
-  const { data, isError, error } = queryResults;
-
-  useEffect(() => {
-    isError && onError(error);
-    data && onSuccess(data);
-
-  }, [isError, error, data, onError, onSuccess]);
-
-  return queryResults;
+  return useQuery('cart-items', fetchCartItems, {
+    onError, onSuccess
+  });
 };
 
 export const addCartItem = (): Promise<CartItem> => ({} as any);
