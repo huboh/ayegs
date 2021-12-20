422cbf4d1ec7bf0cddd03476bd5214ca3f30f9b1
Huboh <72440222+huboh@users.noreply.github.com>
2021-12-20 09:37:37 +0100

    make product.small image component extenable by accepting optional className prop

--- src/components/ProductCardSmall/ProductImage/index.tsx
+++ src/components/ProductCardSmall/ProductImage/index.tsx
@@ -5,14 +5,15 @@ import { Link } from 'react-router-dom';
 
 interface ProductImageProps {
   title: string;
+  className?: string;
   productLink: string;
   mainImageSrc: string;
 }
 
-const ProductImage: FC<ProductImageProps> = ({ mainImageSrc, title, productLink, ...otherProps }) => {
+const ProductImage: FC<ProductImageProps> = ({ mainImageSrc, className = '', title, productLink, ...otherProps }) => {
 
   return (
-    <div className="product-small-image-wrapper" { ...otherProps }>
+    <div className={ `product-small-image-wrapper ${className}`.trim() }{ ...otherProps }>
       <Link to={ productLink } className='image-link-wrapper'>
         <img className='product-image' src={ mainImageSrc } alt={ title } />
       </Link>
