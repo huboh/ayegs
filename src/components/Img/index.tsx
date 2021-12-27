e16b66b79dae996ee4696fcacfc541ce810ac82b
Huboh <72440222+huboh@users.noreply.github.com>
2021-12-27 08:06:58 +0100

    fix Img component & add fallbackimage props rather than fallbacksrc

--- src/components/Img/index.tsx
+++ src/components/Img/index.tsx
@@ -7,28 +7,11 @@ interface ImgProps {
   className: string;
   suspense?: boolean;
   fallBackSrc?: string;
+  fallbackImage?: JSX.Element;
 }
 
-// const Avatar: FC<ImgProps> = (props) => {
-//   const { src, alt, suspense, className, ...otherProps } = props;
-//   const { data: loadedImage } = useQuery([src], () => new Promise<HTMLImageElement>(resolve => {
-//     const image = new Image();
-//     image.onload = () => { resolve(image); };
-//     image.src = src;
-
-//   }), { suspense: true });
-
-//   if (!suspense) {
-//     return <img { ...{ src, className, ...otherProps, alt } } />;
-//   }
-
-//   return !loadedImage ? null : (
-//     <img { ...{ className, ...otherProps, alt, src: loadedImage.src, } } />
-//   );
-// };
-
-const Img: FC<ImgProps> = (props) => {
-  const { src, alt, suspense = true, fallBackSrc, className, ...otherProps } = props;
+const Avatar: FC<ImgProps> = (props) => {
+  const { src, alt, suspense, className, ...otherProps } = props;
   const { data: loadedImage } = useQuery([src], () => new Promise<HTMLImageElement>(resolve => {
     const image = new Image();
     image.onload = () => { resolve(image); };
@@ -40,10 +23,18 @@ const Img: FC<ImgProps> = (props) => {
     return <img { ...{ src, className, ...otherProps, alt } } />;
   }
 
+  return !loadedImage ? null : (
+    <img { ...{ className, ...otherProps, alt, src: loadedImage.src, } } />
+  );
+};
+
+const Img: FC<ImgProps> = (props) => {
+  const { src, fallBackSrc, alt, className, suspense = true, fallbackImage } = props;
+
   // TODO : add error boundary
   return (
-    <Suspense fallback={ <img src={ fallBackSrc ?? src } alt={ alt } className={ className } /> }>
-      { loadedImage && <img { ...{ className, ...otherProps, alt, src: loadedImage.src, } } /> }
+    <Suspense fallback={ fallbackImage ?? <img src={ fallBackSrc ?? src } alt={ alt } className={ className } /> }>
+      <Avatar { ...{ src, className, alt, suspense } } />
     </Suspense>
   );
 };
