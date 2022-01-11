6da1423d557a221ba09c1a8f7776ccfc0a639ac7
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-11 18:57:14 +0100

    FEAT : integrate header hamburger button to modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,15 +5,21 @@ import Logo from "./Logo";
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
 import { HamburgerButton } from './HeaderButtons';
+import useModal from '../../hooks/useModal';
 
 const Header: FC = () => {
+  const { Modal, modalProps, modalHandlers } = useModal({ closeOnOverlayClick: true });
+  const { toggle } = modalHandlers;
 
   return (
     <header className="header">
-      <HamburgerButton style={ { marginRight: '2em' } } onClick={ () => { } } />
+      <HamburgerButton style={ { marginRight: '2em' } } onClick={ () => toggle() } />
       <Logo />
       <SearchBar />
       <NavBar />
+      <Modal { ...modalProps } ModalContent={ (
+        <div>hey there</div>
+      ) } />
     </header>
   );
 };
