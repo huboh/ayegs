0689458114c673c49dc18c9758dc52eb3e27df16
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-11 06:38:09 +0100

    REFACTOR : change to uncontrolled form & moved search icon to searchbar component

--- src/components/Header/SearchBar/SearchForm/index.tsx
+++ src/components/Header/SearchBar/SearchForm/index.tsx
@@ -1,32 +1,48 @@
 import './searchForm.scss';
 import { useRef, FC, SyntheticEvent } from "react";
+import useEventListener from '../../../../hooks/useEventListener';
 
-import { ReactComponent as SearchIcon } from './search.svg';
-
-interface SearchFormProps {
-  placeholder?: string;
+export interface SearchFormProps {
+  searchTerm: string;
+  inputPlaceholder: string;
+  onBlur?: () => void;
+  onFocus?: () => void;
   onSubmit?: (input: string) => void;
   onInput: (input: string) => void;
 }
 
-const formDefaultPlaceHolder = 'search products, brands & category';
-
-const SearchForm: FC<SearchFormProps> = ({ onInput, onSubmit, placeholder = formDefaultPlaceHolder }) => {
+const SearchForm: FC<SearchFormProps> = ({ onInput, onSubmit, inputPlaceholder, onBlur, onFocus, searchTerm }) => {
   const inputRef = useRef<HTMLInputElement>(null);
+
+  useEventListener({
+    target: inputRef,
+    eventType: ['focus', 'blur', 'input'],
+
+    eventHandler(event) {
+      switch (event.type) {
+        case 'blur': return onBlur?.();
+        case 'focus': return onFocus?.();
+        case 'input': return onInput((event as any).target.value);
+        default: break;
+      }
+    }
+  });
 
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
     onSubmit?.(inputRef.current?.value!);
   };
 
-  const handleInput = () => {
-    onInput(inputRef.current?.value!);
-  };
-
   return (
     <form className='search-form' onSubmit={ handleSubmit }>
-      <input type="text" ref={ inputRef } className="search-form-input" placeholder={ placeholder } onInput={ handleInput } />
-      <SearchIcon className='search-icon' />
+      <input
+        type="text"
+        ref={ inputRef }
+        className="search-form-input"
+        placeholder={ inputPlaceholder }
+        value={ searchTerm }
+        onChange={ e => e }
+      />
     </form>
   );
 };
